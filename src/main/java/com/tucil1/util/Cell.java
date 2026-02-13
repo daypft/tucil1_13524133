8af3c97b9d23d225660f3c117e91bd36b8a290de
Muhammad Daffa Arrizki Yanma <dappayanma@gmail.com>
2026-02-13 19:19:42 +0700

    feat: cell.java

---
+++ src/main/java/com/tucil1/util/Cell.java
@@ -0,0 +1,31 @@
+package com.tucil1.util;
+
+public class Cell {
+    private int row;
+    private int col;
+    private char region;
+
+    // Constructor
+    public Cell (int row, int col, char region) {
+        this.row = row;
+        this.col = col;
+        this.region = region;
+    }
+
+    public int getRow(){
+        return this.row;
+    }
+
+    public int getCol(){
+        return this.col;
+    }
+
+    public char getRegion(){
+        return this.region;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Cell[%d,%d](%c)", row, col, region);
+    }
+}
